726f14b91a9ddc48bd11074cf173c722d9a147f1
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-08-14 09:56:41 +0500

    bug fix

--- src/Menu.js
+++ src/Menu.js
@@ -8,7 +8,7 @@ const Menu = ({ items }) => {
           const { id, price, title, img, desc } = item;
           return (
             <article key={id} className="menu-item">
-              <img src={img} alt={title} />
+              <img src={img} alt={title} className="photo" />
               <div className="item-info">
                 <header>
                   <h4>{title}</h4>
