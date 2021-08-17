b27907813fb8695d79deb151d301c9071aefd2e3
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-08-17 16:29:44 +0500

    Structure updated

--- Grocery buds/src/App.js
+++ Grocery buds/src/App.js
@@ -8,7 +8,38 @@ function App() {
   const [isEditing, SetIsEditing] = useState(false);
   const [editId, SetEditId] = useState(null);
   const [alert, SetAlert] = useState({ show: false, msg: "", type: "" });
-  return <h2>grocery bud setup</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+  return (
+    <>
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && <Alert />}
+          <h3>Grocery Items</h3>
+          <div className="form-control">
+            <input
+              type="text"
+              className="grocery"
+              placeholder="Enter any item...."
+              value={name}
+              onChange={(e) => {
+                Setname(e.target.value);
+              }}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? "Edit" : "Submit"}
+            </button>
+          </div>
+        </form>
+        <div className="grocery-container">
+          <List />
+          <button className="clear-btn">clear items</button>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
