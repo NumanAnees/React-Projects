c181439063a65ef527dfba9f3df2ff45a6d0f265
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-08-17 16:05:06 +0500

    App.js updated

--- Grocery buds/src/App.js
+++ Grocery buds/src/App.js
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, Setname] = useState("");
+  const [list, Setlist] = useState([]);
+  const [isEditing, SetIsEditing] = useState(false);
+  const [editId, SetEditId] = useState(null);
+  const [alert, SetAlert] = useState({ show: false, msg: "", type: "" });
+  return <h2>grocery bud setup</h2>;
 }
 
-export default App
+export default App;
