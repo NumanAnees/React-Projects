56036e24419284e7de700e83f502ed651db339d5
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-08-18 12:06:07 +0500

    edit items updated

--- Grocery buds/src/App.js
+++ Grocery buds/src/App.js
@@ -19,6 +19,18 @@ function App() {
       AlertHandle(true, "danger", "Item cannot be empty");
     } else if (name && isEditing) {
       //edit
+      Setlist(
+        list.map((item) => {
+          if (item.id === editId) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      Setname("");
+      SetEditId(null);
+      SetIsEditing(false);
+      AlertHandle(true, "success", "value changed");
     } else {
       AlertHandle(true, "success", "Item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
@@ -33,11 +45,30 @@ function App() {
     AlertHandle(true, "danger", "empty list");
     Setlist([]);
   };
+  const removeHandle = (id) => {
+    AlertHandle(true, "danger", "Item Removed");
+
+    Setlist(
+      list.filter((item) => {
+        return item.id !== id;
+      })
+    );
+  };
+  const editHandle = (id) => {
+    const SpecificItem = list.find((item) => {
+      return item.id === id;
+    });
+    SetIsEditing(true);
+    SetEditId(id);
+    Setname(SpecificItem.title);
+  };
   return (
     <>
       <section className="section-center">
         <form className="grocery-form" onSubmit={handleSubmit}>
-          {alert.show && <Alert {...alert} removeAlert={AlertHandle} />}
+          {alert.show && (
+            <Alert {...alert} removeAlert={AlertHandle} list={list} />
+          )}
           <h3>Grocery Items</h3>
           <div className="form-control">
             <input
@@ -56,7 +87,11 @@ function App() {
         </form>
         {list.length > 0 && (
           <div className="grocery-container">
-            <List items={list} />
+            <List
+              items={list}
+              removeHandle={removeHandle}
+              editHandle={editHandle}
+            />
             <button className="clear-btn" onClick={clearHandle}>
               clear items
             </button>
--- Grocery buds/src/List.js
+++ Grocery buds/src/List.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = ({ items }) => {
+const List = ({ items, removeHandle, editHandle }) => {
   return (
     <div className="grocery-list">
       {items.map((item) => {
@@ -9,10 +9,18 @@ const List = ({ items }) => {
           <article key={id} className="grocery-item">
             <p className="title">{title}</p>
             <div className="btn-container">
-              <button type="button" className="edit-btn">
+              <button
+                type="button"
+                className="edit-btn"
+                onClick={() => editHandle(id)}
+              >
                 <FaEdit />
               </button>
-              <button type="button" className="delete-btn">
+              <button
+                type="button"
+                className="delete-btn"
+                onClick={() => removeHandle(id)}
+              >
                 <FaTrash />
               </button>
             </div>
