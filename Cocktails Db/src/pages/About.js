f288b18a31b77728039c43a9d2ebbdf4dad8d41f
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-08-22 15:39:54 +0500

    About us page updated

--- Cocktails Db/src/pages/About.js
+++ Cocktails Db/src/pages/About.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint dolorem
+        tempora accusantium, praesentium vero iusto deleniti quas magnam odit
+        quibusdam in maxime officia ratione facilis, culpa libero quidem
+        tenetur? Earum! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, atque? Tenetur sed autem, dolorem ullam sit enim reiciendis
+        blanditiis voluptates? Officiis sint veritatis maxime perferendis veniam
+        aliquam consequatur ipsum dicta?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
