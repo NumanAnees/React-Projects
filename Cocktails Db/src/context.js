b2204386a4613ec418c0278dce807fdb5f8a437c
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-08-23 12:59:24 +0500

    api setup

--- Cocktails Db/src/context.js
+++ Cocktails Db/src/context.js
@@ -5,9 +5,41 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [loading, isLoading] = useState(true);
-  const [searchTerm, SetSearchTerm] = useState("a");
+  const [loading, SetLoading] = useState(true);
+  const [searchTerm, SetSearchTerm] = useState("marg");
   const [cocktails, Setcocktails] = useState([]);
+
+  const fetchDrink = async () => {
+    SetLoading(true);
+    try {
+      const res = await fetch(`${url}${searchTerm}`);
+      const data = await res.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        Setcocktails(newCocktails);
+      } else {
+        Setcocktails([]);
+      }
+      SetLoading(false);
+    } catch (e) {
+      console.log(e);
+      SetLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchDrink();
+  }, [SetSearchTerm]);
   return (
     <AppContext.Provider value={{ loading, cocktails, SetSearchTerm }}>
       {children}
