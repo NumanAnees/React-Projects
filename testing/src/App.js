064e788702d2aa5b9a306fb383400629562dadaa
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-06-12 23:50:00 +0500

    Simple counter

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [count,SetCount] = useState(0);
+  useEffect(()=>{
+    console.log("changed")
+  },[count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='text-center'>
+    <h1>{count}</h1>
+    <button onClick={()=>{SetCount(count+1)}}>Increase</button>
+    <button onClick={()=>{SetCount(count-1)}}>Decrease</button>
     </div>
+    </>
   );
 }
 
